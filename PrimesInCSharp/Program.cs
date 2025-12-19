3fbb0bbd4a444810e6dbab5d729e7a07fb1d6662
Freddy JUHEL <PFJL06651@commun.ad.sncf.fr>
2025-12-19 17:23:23 +0100

    Ajoutez des fichiers projet.

---
+++ PrimesInCSharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PrimesInCSharp
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      // time how long it takes to compute primes up to 1 million
+      int limit = 100;
+      Stopwatch chrono = new Stopwatch();
+      chrono.Start();
+      List<int> primes = GetPrimesUpTo(limit);
+      chrono.Stop();
+      Console.WriteLine($"Prime numbers up to {limit}:");
+      Console.WriteLine(string.Join(", ", primes));
+      Display($"Time taken to compute primes up to {limit}: {ToHourMinutesSeconds(chrono.Elapsed)}");
+
+      
+    }
+
+    private static List<int> GetPrimesUpTo(int limit)
+    {
+      List<int> primes = new List<int>();
+      for (int num = 2; num <= limit; num++)
+      {
+        bool isPrime = true;
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+          if (num % i == 0)
+          {
+            isPrime = false;
+            break;
+          }
+        }
+
+        if (isPrime)
+        {
+          primes.Add(num);
+        }
+      }
+
+      return primes;
+    }
+
+    private static string ToHourMinutesSeconds(TimeSpan timeSpan)
+    {
+      return string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D2}",
+          timeSpan.Hours,
+          timeSpan.Minutes,
+          timeSpan.Seconds,
+          timeSpan.Milliseconds);
+    }
+  }
+}
---
+++ PrimesInCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("PrimesInCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("e-SNCF")]
+[assembly: AssemblyProduct("PrimesInCSharp")]
+[assembly: AssemblyCopyright("Copyright © e-SNCF 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7ef0be81-1307-42d6-9d00-6ae08a00e46c")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
