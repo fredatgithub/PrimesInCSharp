65ea9bc9a0c540ff5bae1b64d0bcaea0c0051677
fred <domaine75@gmail.com>
2025-12-28 16:11:52 +0100

    add bit integer code

--- BigIntegerCpp/BigIntegerCpp.cpp
+++ BigIntegerCpp/BigIntegerCpp.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+bool static IsPrime(const cpp_int& number)
+{
+  if (number <= 1) return false;
+  if (number == 2 || number == 3 || number == 5) return true;
+  if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0) return false;
+  
+  cpp_int squareRoot = sqrt(number);
+  for (cpp_int divisor = 7; divisor <= squareRoot; divisor += 2)
+  {
+    if (number % divisor == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
 
 int main()
 {
     std::cout << "Computing big integer primes\n";
 
-    return 0;
+    cpp_int number = 8446744073714979933;
+    if (IsPrime(number))
+    {
+      cout << number << " est premier" << std::endl;
+    }
+    else
+    {
+      cout << number << " n'est pas premier" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
